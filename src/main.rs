03a01456491b6c8d3aa998ab58d9a5e3a7c345aa
André Bacchi <andre.rossini.bacchi@gmail.com>
2026-07-26 00:59:39 -0300

    refactor: create update_snake_pos fn and add some comments to the code

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,21 @@ type BoardState = [[u8; BOARD_SIZE]; BOARD_SIZE];
 type SnekkSize = usize;
 type SnekkPos = (usize, usize);
 
+// Utils
+
 fn clear_terminal() {
   // \x1B[2J clears the screen
   // \x1B[1;1H moves the cursor to row 1, column 1
   print!("\x1B[2J\x1B[1;1H");
 }
+
+// State Updaters
+
+fn update_snake_pos (mut snekk_pos: SnekkPos) -> SnekkPos {
+  let increment: usize = 1;
+
+  return (snekk_pos.0, (snekk_pos.1 + increment) % BOARD_SIZE);
+} 
 
 fn update_board_state(
   snekk_size: SnekkSize,
@@ -36,6 +46,8 @@ fn update_board_state(
   return board_state;
 }
 
+// Render Functions
+
 fn render_snekk_pos(snekk_pos: SnekkPos) {
   println!(" Snekk pos - x: {} y: {}", snekk_pos.0, snekk_pos.1);
 }
@@ -56,13 +68,15 @@ fn render_game(snekk_pos: SnekkPos, board_state: BoardState) {
   render_board(board_state);
 }
 
+// Core Game Loop
+
 fn main() {
   let mut board_state: BoardState = [[0; BOARD_SIZE]; BOARD_SIZE];
   let mut snekk_pos: (usize, usize) = (1, 3);
   let snekk_size: usize = 3; // for now not mut
   
   loop {
-    snekk_pos = (snekk_pos.0, (snekk_pos.1 + 1 as usize)%BOARD_SIZE);
+    snekk_pos = update_snake_pos(snekk_pos);
     board_state = update_board_state(snekk_size, snekk_pos, board_state);
     
     thread::sleep(Duration::from_millis(150));
